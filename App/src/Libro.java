512b865e01818d74714653a8c34001cb1990b4b4
pauletik <pauletik@nobeboaguasolococacola.com>
2025-06-03 00:25:59 +0200

    añadido fecha publi al libro y isbn a int

--- App/src/Libro.java
+++ App/src/Libro.java
@@ -2,25 +2,26 @@ public class Libro {
 
         private String titulo;
         private String autor;
-        private String isbn;
+        private int isbn;
+        private String fecha_publi;
 
-        public Libro(String titulo, String autor, String isbn) {
+        public Libro(String titulo, String autor, int isbn, String fecha_publi) {
             this.titulo = titulo;
             this.autor = autor;
             this.isbn = isbn;
+            this.fecha_publi = fecha_publi;
         }
 
         public String getTitulo() { return titulo; }
         public String getAutor() { return autor; }
-        public String getIsbn() { return isbn; }
+        public int getIsbn() { return isbn; }
 
         public void setTitulo(String titulo) { this.titulo = titulo; }
         public void setAutor(String autor) { this.autor = autor; }
-
-        @Override
-        public String toString() {
-            return "[" + isbn + "] \"" + titulo + "\" por " + autor;
+        public void setIsbn(int isbn) {
+            this.isbn = isbn;
         }
+
     }
 
 
